0e6b4c42363621facb1eedddf732a68c74444fa9
Warren Hansen <whansen1@mac.com>
2018-02-24 17:03:40 -0800

    added

--- LoadCSV.cs
+++ LoadCSV.cs
@@ -35,17 +35,24 @@ namespace NinjaTrader.NinjaScript.Indicators
 		
 		private struct TradeResults
 		{
-			/// Date, Ticker, profit, exit name, profit factor, Consecutive losers, largest loser, largest winner, profit per month
-			public  string 	date 	{ get; set; }
+			/// Trade Count, Date In, Date Out, Ticker, profit, cumProfit,
+			public  int 	tradeNum 	{ get; set; }
+			public  string 	dateEntry 	{ get; set; }
+			public  string 	dateExit 	{ get; set; }
 			public  string 	ticker 	{ get; set; }
 			public  double 	profit 	{ get; set; }
-//			public  string 	exitName 	{ get; set; }
-//			public  double 	profitFactor 	{ get; set; }
+			public  double 	cumProfit 	{ get; set; }
 			
-//			public  double 	consecutiveLosers 	{ get; set; }
-//			public  double 	largestLoser 	{ get; set; }
-//			public  double 	largestWinner 	{ get; set; }
-//			public  double 	profitPerMonth 	{ get; set; }
+			/// exit name, profit factor, winPct, Consecutive losers, 
+			public  string 	exitName 	{ get; set; }
+			public  double 	profitFactor 	{ get; set; }
+			public  double 	winPct 	{ get; set; }
+			public  int 	consecutiveLosers 	{ get; set; }
+			
+			/// largest loser, largest winner, profit per month
+			public  double 	largestLoser 	{ get; set; }
+			public  double 	largestWinner 	{ get; set; }
+			public  double 	profitPerMonth 	{ get; set; }
 		}
 		
 		private TradeResults tradeResults = new TradeResults{};
@@ -96,12 +103,60 @@ namespace NinjaTrader.NinjaScript.Indicators
 		private void createStructFrom(string[] oneFile, bool debug) {
 
 			foreach (String rows in oneFile) {		
-				//Print(lines);
+				//Print(rows);
+				/// Trade Count, Date In, Date Out, Ticker, lastProfitCurrency, cumProfit, exit name, profit factor, winPct Consecutive losers, largest loser, largest winner, profit per month
 				var columns = rows.Split(',');
-				if ( debug ) { Print(columns[0] +"\t"+ columns[1] +"\t"+ columns[2] +"\t"); }
-				tradeResults.date = columns[0];
-				tradeResults.ticker = columns[1];
-				tradeResults.profit = Convert.ToDouble( columns[2]);
+				if ( debug ) { Print(
+					columns[0] +"\t"+ 	// count
+					columns[1] +"\t"+ 	// Date In
+					columns[2] +"\t" + 	// Date out
+					columns[3] +"\t"+ 	// Ticker
+					columns[4] +"\t"+ 	// profit
+					columns[5] +"\t" + 	// cumulative
+					columns[6] +"\t" + 	// exit name
+					columns[7] +"\t"+ 	// pf
+					columns[8] +"\t"+ 	// winPct
+					columns[9] +"\t" + 	// consec loser
+					columns[10] +"\t"+	// LL
+					columns[11] +"\t"+ 	// LW
+					columns[12] +"\t"); } // monthlyProfit
+				
+				/*
+				/// Trade Count, Date In, Date Out, Ticker, profit, cumProfit,
+			public  int 	tradeNum 	{ get; set; }
+			public  string 	dateEntry 	{ get; set; }
+			public  string 	dateExit 	{ get; set; }
+			public  string 	ticker 	{ get; set; }
+			public  double 	profit 	{ get; set; }
+			public  double 	cumProfit 	{ get; set; }
+			
+			/// exit name, profit factor, winPct, Consecutive losers, 
+			public  string 	exitName 	{ get; set; }
+			public  double 	profitFactor 	{ get; set; }
+			public  double 	winPct 	{ get; set; }
+			public  int 	consecutiveLosers 	{ get; set; }
+			
+			/// largest loser, largest winner, profit per month
+			public  double 	largestLoser 	{ get; set; }
+			public  double 	largestWinner 	{ get; set; }
+			public  double 	profitPerMonth 	{ get; set; }
+				*/
+				tradeResults.tradeNum = Convert.ToInt16(columns[0]);
+				tradeResults.dateEntry = columns[1];
+				tradeResults.dateExit = columns[2];
+				tradeResults.ticker = columns[3];		
+				tradeResults.profit = Convert.ToDouble( columns[4]);
+				tradeResults.cumProfit = Convert.ToDouble( columns[5]); 
+				
+				tradeResults.exitName = columns[6];
+				tradeResults.profitFactor = Convert.ToDouble( columns[7]); 
+				tradeResults.winPct = Convert.ToDouble( columns[8]); 		
+				tradeResults.consecutiveLosers = Convert.ToInt16(columns[9]);
+
+				tradeResults.largestLoser = Convert.ToDouble( columns[10]);
+				tradeResults.largestWinner = Convert.ToDouble( columns[11]);
+				tradeResults.profitPerMonth = Convert.ToDouble( columns[12]);
+				
 				allTradeResults.Add(tradeResults);
 				
 			}
@@ -113,7 +168,11 @@ namespace NinjaTrader.NinjaScript.Indicators
 				Print("\n"+ filesParsedCount + " of " + fileCount + " ticker backtest files parsed\n");
 				Print("\nFinished with allTradeResults...");
 				foreach (TradeResults thing in allTradeResults) {	
-					Print(thing.date +" \t\t\t"+ thing.ticker +"\t\t\t"+ thing.profit);
+					Print(thing.tradeNum +" \t\t\t"+ thing.dateEntry +"\t\t\t"+ thing.dateExit +" \t\t\t"+ thing.ticker +"\t\t\t"+ thing.profit + "\t\t\t"+ thing.cumProfit +
+					" \t\t\t"+ thing.exitName +"\t\t\t"+ thing.profitFactor +" \t\t\t"+ thing.winPct +"\t\t\t"+ thing.consecutiveLosers +
+					"\t\t\t"+ thing.largestLoser +" \t\t\t"+ thing.largestWinner +"\t\t\t"+ thing.profitPerMonth
+					
+					);
 				}
 			}
 		}
